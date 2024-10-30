5fff9e138f39b40b2359c60029e1bfce5015aa43
Avantiermv <avantiermv@gmail.com>
2024-10-30 19:23:25 -0300

    Fifth commit of this project.
    I changed the id from some inputs, because they're wrong, and I addicted the js code.

---
+++ logic.js
@@ -0,0 +1,9 @@
+let firstname = document.getElementById('firstname');
+let lastname = document.getElementById('lastname');
+let addressemail = document.getElementById('emailaddress');
+
+
+
+function start(){
+    
+}
