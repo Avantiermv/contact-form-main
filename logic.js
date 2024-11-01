315087ff4b3d614443e5311c3a6672578551da0a
Avantiermv <avantiermv@gmail.com>
2024-10-31 21:47:40 -0300

    Seventh commit of this challenge.
    In this commit, you can see more changes in the js code. For example, I changed completely the js code.

--- logic.js
+++ logic.js
@@ -1,9 +1,15 @@
-const firstname = document.getElementById('firstname');
-const lastname = document.getElementById('lastname');
-const addressemail = document.getElementById('emailaddress');
-
+const inputsname = [
+    {element: document.getElementById('firstname'), name: 'First Name'},
+    {element: document.getElementById('lastname'), name: 'Last Name'}
+]
+const regex = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/;
 
 
 function start(){
-   
+  for(let i=0;i<inputsname.length;i++){
+    let input = inputsname[i];
+    if(input.element.value === ''){
+        window.alert(`${input.name} não está preenchido`);
+    }
+  }
 }
