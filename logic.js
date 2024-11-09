b97011dbd8551a2ddb0af8089c34eca1144b5477
Avantiermv <avantiermv@gmail.com>
2024-11-09 20:42:18 -0300

    Thirteenth commit of this challenge.
    In this commit, I made more changes specially in JS code. I addicted three more variables and changed the conditionals.

--- logic.js
+++ logic.js
@@ -2,10 +2,12 @@ const inputsname = [
     {element: document.getElementById('firstname'), name: 'First Name'},
     {element: document.getElementById('lastname'), name: 'Last Name'}
 ];
-const emailadress = document.getElementById('emailaddress');
+const emailaddress = document.getElementById('emailaddress');
 const spans = document.getElementsByTagName('span');
 const query1 = document.getElementById('generalenquiry');
 const query2 = document.getElementById('supportrequest');
+const msg = document.getElementById('messagebox');
+const consent = document.getElementById('consent');
 
 function start(){
 
@@ -25,20 +27,51 @@ function start(){
 
     if(input.element.value === '' || !validateName(input.element.value)){
         input.element.style.borderColor="red";
-        span.style.color="red"
+        span.style.color="red";
+        hasError = true;
+    }else{
+      input.element.style.borderColor="";
+      span.style.color="";
     }
 
-    if(emailadress.value === '' || !validateEmail(emailadress.value)){
-      emailadress.style.borderColor="red"; 
-      span.style.color="red";
+    let emailSpan = spans[inputsname.length];
+    if(emailaddress.value === '' || !validateEmail(emailaddress.value)){
+      emailaddress.style.borderColor="red"; 
+      emailSpan.style.color="red";
+      window.alert("Tem npumero no teu nome");
+      hasError = true;
+    }else{
+      emailaddress.style.borderColor=""; 
+      emailSpan.style.color="";
     }
  
-   if(!query1.checked || !query2.checked){
-     span.style.color="red";
-     window.alert("Bolinha nãp preenchdia");
-   }
+    let querySpan = spans[inputsname.length + 1];
+    if(!query1.checked || !query2.checked){
+     querySpan.style.color="red";
+     hasError = true;
+    }else{
+      querySpan.style.color="";
+    } 
 
+    let msgSpan = spans[inputsname.length + 2];
+    if(msg.value === ''){
+      msg.style.borderColor="red";
+      msgSpan.style.color="red";
+      hasError = true;
+    }else{
+      msg.style.borderColor="";
+      msgSpan.style.color="";
+    }
 
+    let consentSpan = spans[inputsname.length + 3];
+    if(!consent.checked){
+      consent.style.borderColor="red";
+      consentSpan.style.color="red";
+      hasError = true;
+    }else{
+      consent.style.borderColor="";
+      consentSpan.style.color="";
+    }
   }
 
 
