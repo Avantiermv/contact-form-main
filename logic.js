098ddb357d6afed53149c1f01c83743de94f8dd8
Avantiermv <avantiermv@gmail.com>
2024-11-10 20:47:37 -0300

    .

--- logic.js
+++ logic.js
@@ -4,13 +4,14 @@ const inputsname = [
 ];
 const emailaddress = document.getElementById('emailaddress');
 const spans = document.getElementsByTagName('span');
+const spanError = document.getElementsByClassName('spanError');
 const query1 = document.getElementById('generalenquiry');
 const query2 = document.getElementById('supportrequest');
+const queryspanerror = document.getElementById('queryspanerror');
 const msg = document.getElementById('messagebox');
 const consent = document.getElementById('consent');
 
 function start(){
-
   function validateEmail(email){
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
@@ -21,52 +22,59 @@ function start(){
     return regex.test(name);
   }
 
+  let hasError = false;
+
   for(let i=0;i<inputsname.length;i++){
     let input = inputsname[i];
-    let span = spans[i];
 
+    let span = input.element.nextElementSibling;
     if(input.element.value === '' || !validateName(input.element.value)){
         input.element.style.borderColor="red";
         span.style.color="red";
+        span.style.display="Block";
         hasError = true;
     }else{
       input.element.style.borderColor="";
       span.style.color="";
     }
 
-    let emailSpan = spans[inputsname.length];
+    let emailSpan = emailaddress.nextElementSibling;
     if(emailaddress.value === '' || !validateEmail(emailaddress.value)){
       emailaddress.style.borderColor="red"; 
       emailSpan.style.color="red";
-      window.alert("Tem npumero no teu nome");
+      emailSpan.style.display="block";
       hasError = true;
     }else{
       emailaddress.style.borderColor=""; 
       emailSpan.style.color="";
     }
  
-    let querySpan = spans[inputsname.length + 1];
+    let querySpan = spans[inputsname.length];
+    let queryerror = queryspanerror;
     if(!query1.checked || !query2.checked){
-     querySpan.style.color="red";
-     hasError = true;
+      querySpan.style.color="red";
+      queryerror.style.display="block";
+      hasError = true;
     }else{
       querySpan.style.color="";
     } 
 
-    let msgSpan = spans[inputsname.length + 2];
+    let msgSpan = document.getElementById('messagebox').nextElementSibling;
     if(msg.value === ''){
       msg.style.borderColor="red";
       msgSpan.style.color="red";
+      msgSpan.style.display="block";
       hasError = true;
     }else{
       msg.style.borderColor="";
       msgSpan.style.color="";
     }
 
-    let consentSpan = spans[inputsname.length + 3];
+    let consentSpan = consent.nextElementSibling.nextElementSibling;
     if(!consent.checked){
       consent.style.borderColor="red";
       consentSpan.style.color="red";
+      consentSpan.style.display="block";
       hasError = true;
     }else{
       consent.style.borderColor="";
