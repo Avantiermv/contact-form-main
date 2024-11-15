849af8ecda09f5a9faf606d48155bd6f234acdc1
Avantiermv <avantiermv@gmail.com>
2024-11-15 15:39:35 -0300

    Commit of the new functions.
    In this commit, you can see some changes, in fact, a lot of changes in my code. I took some conditionals and change some variables.

--- logic.js
+++ logic.js
@@ -18,13 +18,77 @@ function start(){
     const regex = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]+$/;
     return regex.test(name);
   }
+
+  let hasError = false;
+  
  
-  if(inputsname === '' || !validateName(inputsname)){
-    inputsname.style.color="red";
+ for(let input of inputsname){
+  let spansn = document.getElementsByClassName('namespan');
+  let spanErrorName = document.getElementsByClassName('spanErrorName');
+  if(input.element.value.trim() === '' || !validateName(input.element.value)){
+    input.element.style.borderColor="red";
+    for(let span of spansn){
+      span.style.color="red";
+    }
+    for(let spanN of spanErrorName){
+      spanN.style.display="Block"
+      spanN.style.color="red";
+    }
+    hasError = true;
+    break;
+  }else{
+    input.element.style.borderColor="";
+    for(let span of spansn){
+      span.style.color="";
+    }
+    for(let spanN of spanErrorName){
+      spanN.style.display=""
+      spanN.style.color="";
+    }
+  }
+ }
+
+ let spane = document.getElementById('emailspan');
+  if(emailaddress.value.trim() === '' || !validateEmail(emailaddress)){
+    emailaddress.style.borderColor="red";
+    spane.style.color="red";
+    hasError = true;
+  }else{
+    emailaddress.style.borderColor="";
+    spane.style.color="";
   }
 
-  if(emailaddress === '' || !validateEmail(emailaddress)){
-    window.alert("Algo deu errado");
+ let spanq = document.getElementById('queryspan');
+  if(!query1.checked || !query2.checked){
+    spanq.style.color="red";
+    hasError = true;
+  }else{
+    spanq.style.color="";
   }
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  if(hasError == false){
+    window.alert("Tudo certo");
+  }
 }
